c02d6bd02c98a482a9cab19fd2c26a03cc605af5
Fathimashahanaip <shahanaip222@gmail.com>
2023-02-16 12:28:51 +0530

    first

---
+++ re-app/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Fir from './Fir';
+import Array from './Array';
+import Arrobj from './Arrobj';
+import Barnav from './Barnav';
+import Arrcard from './Arrcard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Fir/>
+    <Array />
+    <Arrobj />
+    <Barnav />
+    <Arrcard />
+    
+
+    
+  </React.StrictMode>
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
